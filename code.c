512127849b848056d5fcb9ac574c39376bdb6ba0
Pratik Chapagain <chapagainpratik28@gmail.com>
2024-01-06 07:53:56 +0545

    push

---
+++ code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int tp,rs,ep;
+	printf("Enter Total Paisa : ");
+	scanf("%d",&tp);
+	rs = tp / 100;
+	ep = tp % 100;
+	printf("\nTotal Paisa = %d",tp);
+	printf("\nConverted Paisa in Rs = %d.%d",rs,ep);
+
+	return 0;
+}
